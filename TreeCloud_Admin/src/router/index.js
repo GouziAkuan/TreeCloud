ecea3e0300589c60acd24bc934ddc2f58163d3a0
akuan <3109197060@qq.com>
2024-09-18 19:27:19 +0800

    更新管理系统路由模式

--- TreeCloud_Admin/src/router/index.js
+++ TreeCloud_Admin/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { adminTokenStore } from '@/stores/token'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
